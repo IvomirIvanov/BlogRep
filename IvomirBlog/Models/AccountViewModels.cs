5b81689704b599bb0b65f56518bd49c9a90d96f5
IvomirIvanov <mmoacc@abv.bg>
2016-08-30 17:22:00 +0300

    Implementing posts
    Implementing posts and posts controller

--- IvomirBlog/Models/AccountViewModels.cs
+++ IvomirBlog/Models/AccountViewModels.cs
@@ -70,7 +70,7 @@ namespace IvomirBlog.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
@@ -79,6 +79,11 @@ namespace IvomirBlog.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        
+        
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
     }
 
     public class ResetPasswordViewModel
@@ -89,7 +94,7 @@ namespace IvomirBlog.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
